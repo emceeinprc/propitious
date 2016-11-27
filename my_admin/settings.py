ae57204e6dbe50318d444814b25b2c65d1edc54e
emceeinprc <matt@seekpanda.com>
2016-11-27 18:18:56 +0800

    Added whitenoise

--- my_admin/settings.py
+++ my_admin/settings.py
@@ -129,3 +129,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     os.path.join(PROJECT_ROOT, 'static'),
 ]
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
